dd8ace56594e9adf724a84fb38c573ab55b5ea02
unknown <ateniolataye@gmail.com>
2026-05-06 14:59:26 +0100

    added

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 //import express
-const express = require('express');
-//import cors
-const cors = require('cors');
+const express = require("express");
+//cors
+const cors = require("cors");
+//mongoose
+const mongoose = require("mongoose");
+const router = require("./routes/userRoutes");
+
+const live_url =
+  "mongodb+srv://dev_esther:dev_esther2000@cluster0.xu8lvjr.mongodb.net/UserDB?appName=Cluster0";
+const local_url = "mongodb://localhost:27017/userDB";
+
+mongoose
+  .connect(live_url)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.error("Connection Error: ", err));
 
 //create express app
 const app = express();
@@ -9,11 +21,13 @@ const port = 7777;
 app.use(cors());
 app.use(express.json());
 
+//use routes
+app.use("/api", router);
 
-app.length('/', (req, res) => {
-    res.send('Api is ready for use');
-}
+app.get("/", (req, res) => {
+  res.send("api is ready for use");
+});
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-}
+  console.log(`server is running on port ${port}`);
+});
